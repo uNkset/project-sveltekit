8b129db9cd42071c028bc5df3136dd60c3dffd49
uNkset <kostylo@windowslive.com>
2023-06-23 13:25:12 +0200

    newsletter sample done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
+	// import { onMount, onDestroy } from 'svelte';
 
-	onMount(() => console.log('Mounted home page'));
-	onDestroy(() => console.log('Destroyed home page'));
+	// onMount(() => console.log('Mounted home page'));
+	// onDestroy(() => console.log('Destroyed home page'));
+	async function subscribe(event: Event) {
+		const form = event.target as HTMLFormElement;
+		const data = new FormData(form);
+
+		await fetch('/api/newsletter', {
+			method: 'POST',
+			body: data,
+		});
+	}
 </script>
 
-<h1>Home Page</h1>
+<h1>Newsletter</h1>
+
+<form on:submit|preventDefault={subscribe}>
+	<input type="email" name="email" />
+	<button>Subscribe</button>
+</form>
