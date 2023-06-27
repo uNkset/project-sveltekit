e6bc04ba5fb57899715a5041dadbe8926bda14df
uNkset <kostylo@windowslive.com>
2023-06-27 14:11:44 +0200

    displaying posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,52 @@
 <script lang="ts">
-	// import { onMount, onDestroy } from 'svelte';
+  // import { onMount, onDestroy } from 'svelte';
 
-	// onMount(() => console.log('Mounted home page'));
-	// onDestroy(() => console.log('Destroyed home page'));
-	async function subscribe(event: Event) {
-		const form = event.target as HTMLFormElement;
-		const data = new FormData(form);
+  // onMount(() => console.log('Mounted home page'));
+  // onDestroy(() => console.log('Destroyed home page'));
+  // async function subscribe(event: Event) {
+  // 	const form = event.target as HTMLFormElement;
+  // 	const data = new FormData(form);
 
-		await fetch('/api/newsletter', {
-			method: 'POST',
-			body: data,
-		});
-	}
+  // 	await fetch('/api/newsletter', {
+  // 		method: 'POST',
+  // 		body: data,
+  // 	});
+  // }
+
+  import type { Post } from '@prisma/client'
+
+  async function getPosts() {
+    const response = await fetch('api/posts')
+    const posts: Post[] = await response.json()
+
+    return posts
+  }
 </script>
 
-<h1>Newsletter</h1>
+<h1>Posts</h1>
+
+{#await getPosts()}
+  <p>Loading...</p>
+{:then posts}
+  <p>Showing {posts.length} posts</p>
+
+  {#each posts as { slug, title }}
+    <ul>
+      <li>
+        <a href="/posts/{slug}">{title}</a>
+      </li>
+    </ul>
+  {/each}
+  <!-- <pre>
+		{JSON.stringify(posts, null, 2)}
+	</pre> -->
+{:catch error}
+  <p>error.message</p>
+{/await}
+
+<!-- <h1>Newsletter</h1>
 
 <form on:submit|preventDefault={subscribe}>
 	<input type="email" name="email" />
 	<button>Subscribe</button>
-</form>
+</form> -->
--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,8 +3,16 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import db from '$lib/database';
 
-export const GET: RequestHandler = async () => {
-	const posts = await db.post.findMany();
+export const GET: RequestHandler = async (event) => {
+	const posts = await db.post.findMany({
+		// get random numbers of posts to test caching
+		take: Math.round(Math.random() * 30),
+	});
+
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching
+	event.setHeaders({
+		'Cache-Control': 'max-age=60', // for server-side rendering 'public, max-age=0, s-maxage=60' -- cdn
+	});
 
 	return json(posts);
 };
