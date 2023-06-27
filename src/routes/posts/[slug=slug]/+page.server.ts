7ad9915751944f004b30ff0b24e96195f8f8629e
uNkset <kostylo@windowslive.com>
2023-06-27 14:57:42 +0200

    singe post endpoint from prisma using ssr

---
+++ src/routes/posts/[slug=slug]/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from './$types'
+import { error } from '@sveltejs/kit'
+import db from '$lib/database'
+
+export const load: PageServerLoad = async ({ params }) => {
+  const post = await db.post.findUnique({
+    where: { slug: params.slug },
+  })
+
+  if (!post) {
+    throw error(404, 'Post not found')
+  }
+
+  return { post }
+}
--- src/routes/posts/[slug=slug]/+page.svelte
+++ src/routes/posts/[slug=slug]/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Post page</h1>
+<script lang="ts">
+  // only for server side running like fetching data form DB
+  import type { PageData } from './$types'
+
+  export let data: PageData
+
+  function formatDate(date: Date) {
+    return new Intl.DateTimeFormat('en', { dateStyle: 'long' }).format(date)
+  }
+</script>
+
+<hgroup>
+  <h1>{data.post.title}</h1>
+  <h2>{formatDate(data.post.createdAt)}</h2>
+</hgroup>
+
+<div class="content">
+  {@html data.post.content}
+</div>
