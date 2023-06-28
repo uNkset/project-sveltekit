efbab1ca8a3b2f52eccb88b4c96e897c91d2edcb
uNkset <kostylo@windowslive.com>
2023-06-28 11:21:35 +0200

    displaying todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
 </script>
 
-<h1>Home</h1>
+<h1>Forms</h1>
+
+<form action="/login" method="POST">
+  <input type="text" name="user" />
+  <input type="password" name="password" />
+  <button type="submit">Login</button>
+</form>
---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  export let data: PageData
+
+  async function addTodo(event: Event) {}
+
+  async function removeTodo(event: Event) {}
+</script>
+
+<ul>
+  {#each data.todos as todo}
+    <li>
+      <span>{todo.text}</span>
+      <form on:submit|preventDefault={removeTodo} method="POST">
+        <input type="hidden" name="id" value={todo.id} />
+        <button class="delete" type="submit">X</button>
+      </form>
+    </li>
+  {/each}
+</ul>
+
+<form on:submit|preventDefault={addTodo} method="POST">
+  <input type="text" name="todo" />
+  <button type="submit">Add</button>
+</form>
+
+<style>
+  ul {
+    padding: 0;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  span {
+    text-transform: capitalize;
+  }
+
+  .delete {
+    margin: 0;
+    background: none;
+    border: none;
+  }
+
+  .error {
+    color: tomato;
+  }
+
+  li form button {
+    color: blueviolet;
+  }
+</style>
