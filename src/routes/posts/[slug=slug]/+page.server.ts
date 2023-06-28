32ae31f10bb601298dcac938395ea169634447db
uNkset <kostylo@windowslive.com>
2023-06-28 10:22:22 +0200

    rerun load function example

--- src/routes/posts/[slug=slug]/+page.server.ts
+++ src/routes/posts/[slug=slug]/+page.server.ts
@@ -2,7 +2,10 @@ import type { PageServerLoad } from './$types'
 import { error } from '@sveltejs/kit'
 import db from '$lib/database'
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params /*parent*/ }) => {
+  // const parentData = await parent();
+  // console.log(parentData)
+
   const post = await db.post.findUnique({
     where: { slug: params.slug },
   })
