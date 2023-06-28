6e4020969db2ab08c31c2b7b51f3304e8692e268
uNkset <kostylo@windowslive.com>
2023-06-28 09:10:20 +0200

    page store with seo example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
-	import '../app.css';
+  import { page } from '$app/stores'
+
+  import '../app.css'
 </script>
 
+<!-- <pre>
+  {JSON.stringify($page, null, 2)}
+</pre> -->
+
+<svelte:head>
+  <title>{$page.data.post?.title}</title>
+</svelte:head>
+
 <nav>
-	<ul>
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/posts">Posts</a>
-		</li>
-	</ul>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/posts">Posts</a>
+    </li>
+  </ul>
 </nav>
 
 <slot />
