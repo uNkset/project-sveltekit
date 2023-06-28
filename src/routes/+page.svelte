5c2106b6c772e7dd988c57ce63fd4a0e9ecef6a8
uNkset <kostylo@windowslive.com>
2023-06-28 15:07:58 +0200

    submitting form from other places

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+  import { applyAction, enhance } from '$app/forms'
+  import type { SubmitFunction } from '@sveltejs/kit'
+  import type { ActionData } from './$types'
+
+  export let form: ActionData
+
+  const login: SubmitFunction = () => {
+    return async ({ result }) => {
+      await applyAction(result)
+    }
+  }
 </script>
 
-<h1>Forms</h1>
+<form method="post" action="/login" use:enhance={login}>
+  <input type="text" name="user" value={form?.data?.user ?? ''} />
+  {#if form?.errors?.user}
+    <p class="error">Name is required</p>
+  {/if}
 
-<form action="/login" method="POST">
-  <input type="text" name="user" />
   <input type="password" name="password" />
+  {#if form?.errors?.password}
+    <p class="error">Password is required</p>
+  {/if}
+
   <button type="submit">Login</button>
 </form>
+
+<style>
+  .error {
+    color: tomato;
+  }
+</style>
