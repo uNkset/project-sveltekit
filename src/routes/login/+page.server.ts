ed611daaf6fd6c690cb0e77a42c96b39b39e5e53
uNkset <kostylo@windowslive.com>
2023-06-28 14:55:54 +0200

    validating using zod

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,29 +1,25 @@
 import { fail, type Actions, redirect } from '@sveltejs/kit'
+import { zfd } from 'zod-form-data'
 
 export const actions: Actions = {
   default: async ({ request }) => {
     const formData = await request.formData()
 
-    const user = String(formData.get('user'))
-    const password = String(formData.get('password'))
+    const loginSchema = zfd.formData({
+      user: zfd.text(),
+      password: zfd.text(),
+    })
 
-    const errors: Record<string, unknown> = {}
+    const result = loginSchema.safeParse(formData)
 
-    if (!user || typeof user !== 'string') {
-      errors.user = 'required'
-    }
-
-    if (!password || typeof password !== 'string') {
-      errors.password = 'required'
-    }
-
-    if (Object.keys(errors).length > 0) {
+    if (!result.success) {
       const data = {
         data: Object.fromEntries(formData),
-        errors,
+        errors: result.error.flatten().fieldErrors,
       }
       return fail(400, data)
     }
+
     throw redirect(303, '/todos')
   },
 }
