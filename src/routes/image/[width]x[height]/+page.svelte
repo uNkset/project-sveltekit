ef5f25f2760574e5690bfd7c848351c41de8c088
uNkset <kostylo@windowslive.com>
2023-06-29 09:56:42 +0200

    reset

--- src/routes/image/[width]x[height]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-</script>
-
-<h1>Dimensions</h1>
-
-<pre>
-    {JSON.stringify($page, null, 2)} 
-</pre>
--- src/routes/posts/+page.svelte
+++
@@ -1,7 +0,0 @@
-<h1>Posts</h1>
-
-<!-- <p>
-	you can <a href="/posts/create">+create</a> or edit your posts here.
-</p> -->
-
-<p>Select post to display.</p>
