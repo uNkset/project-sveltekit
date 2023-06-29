ebc6c3d3df78a6c37752e460cd88652c74b7c5da
uNkset <kostylo@windowslive.com>
2023-06-29 11:13:26 +0200

    reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-
   import '../app.css'
 </script>
-
-<!-- <pre>
-  {JSON.stringify($page, null, 2)}
-</pre> -->
-
-<svelte:head>
-  <title>{$page.data.post?.title}</title>
-</svelte:head>
 
 <nav>
   <ul>
     <li>
       <a href="/">Home</a>
-    </li>
-    <li>
-      <a href="/posts">Posts</a>
     </li>
   </ul>
 </nav>
